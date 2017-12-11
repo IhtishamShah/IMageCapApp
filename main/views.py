78e28c3171a2c859a3d050cea7ff39560b8a0403
Ali Baig <ali.baig1996@gmail.com>
2017-12-11 22:14:01 +0500

    Added Neural Talk 2 API implementation

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-
+import requests
+import os
 
 def index(request):
 	if request.method == 'POST' and request.FILES['myfile']:
@@ -11,6 +12,22 @@ def index(request):
 		fs = FileSystemStorage()
 		filename = fs.save(myfile.name, myfile)
 		uploaded_file_url = fs.url(filename)
+
+		img = open(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\\media\\" + myfile.name, "rb")
+
+		r = requests.post(
+		    "https://api.deepai.org/api/neuraltalk",
+		    files={
+		    
+		    'image': img
+		      
+		    
+		    },
+		    headers={'api-key': '17b00d3b-69db-49f5-8931-1c4dc0945e25'}
+		)
+
+		print(r.json()['output'])
+		
 		return render(request, 'main/index.html', {
 		    'uploaded_file_url': uploaded_file_url
 		    })
