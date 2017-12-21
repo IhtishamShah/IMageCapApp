49d650a030358c09b5a35a6a94f6abd5609fed44
IhtishamShah <ihisham.shah48@gmail.com>
2017-12-22 00:00:37 +0500

    UI improvements

--- AwesomeProject/camera.js
+++ AwesomeProject/camera.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, ActivityIndicator, } from 'react-native';
+import { Text, View, TouchableOpacity, ActivityIndicator, StyleSheet} from 'react-native';
 import { Camera, Permissions } from 'expo';
 import { Button } from 'react-native-elements';
 import PopupDialog, {ScaleAnimation,  DialogTitle} from 'react-native-popup-dialog';
@@ -40,6 +40,8 @@ export default class CameraExample extends React.Component {
   if (this.camera) {
     let photo = await this.camera.takePictureAsync(); 
     console.log(photo.uri)
+   
+// **********
     let body = new FormData();
 
     body.append('photo', {uri: photo.uri, name: photo.uri, type:'image/jpg'});
@@ -62,7 +64,8 @@ export default class CameraExample extends React.Component {
                   })
     .catch((e) => console.log(e))
     .done()
-
+// ************
+  // this.setState({caption: photo.uri, loadingAnimation: false})
     
     // setInterval(() => {
     //   this.setState({
@@ -94,7 +97,7 @@ export default class CameraExample extends React.Component {
                 justifyContent: 'center'
 
               }}>
-              <View style={{ alignSelf: 'flex-end'}}>
+              <View style={{ alignSelf: 'flex-end', marginBottom: 10}}>
               <Button 
                  onPress={ () => {
                   this.setState({loadingAnimation: true})
@@ -102,25 +105,28 @@ export default class CameraExample extends React.Component {
                   this.popupDialog.show();
 
                  }}
+                  large
                   raised
                   title='Take Picture'
                   backgroundColor='#397af8'
-                style={{ alignSelf: 'flex-end', alignItems: 'center',}}
+                  icon={{name: 'camera'}}
+                style={{ alignSelf: 'flex-end', alignItems: 'center'}}
               />
               </View>
 
                <PopupDialog
                 ref={(popupDialog) => { this.popupDialog = popupDialog; }}
                 dialogAnimation={scaleAnimation}
+                dialogTitle={<DialogTitle haveTitleBar={false} titleTextStyle={styles.resultTitle} title="Result" />}
+
                 // dialogTitle={<DialogTitle title="Popup Dialog - Scale Animation" />}
                 width = {0.75}
-
+                dialogStyle={styles.popBox}
               >
                 <View>
                 { this.state.loadingAnimation
-                  ? <ActivityIndicator size="large" color="#0000ff" />
-                  : <Text>{this.state.caption}</Text>
-                  
+                  ? <ActivityIndicator size="large" color="#3E4348" />
+                  : <Text style={styles.results}>{this.state.caption}</Text>
                 }
                 </View>
               </PopupDialog>
@@ -131,3 +137,25 @@ export default class CameraExample extends React.Component {
     }
   }
 }
+
+
+const styles = StyleSheet.create({
+  popBox: {
+    padding: 5,
+    paddingRight:10,
+    paddingLeft:10,
+    backgroundColor: '#e1eef6'
+  },
+  results: {
+    fontSize: 15,
+    // fontWeight: 'bold',
+    // margin: 10,
+    color: '#282c37',
+  },
+  resultTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    // margin: 5
+    color: '#004e66',
+  }
+});
